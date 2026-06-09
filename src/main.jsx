a8916e61f76a17ee801b675a3aafbd32290d87db
Costincianu Gheorghina <gheorghina.costincianu@sfr.fr>
2026-06-10 00:03:42 +0200

    correction configuration Sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -9,18 +8,12 @@ import * as Sentry from "@sentry/react";
 // =====================
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
-  // Active le monitoring uniquement en production
-  enabled: import.meta.env.PROD,
-  // Taux d'échantillonnage des performances (10%)
-  tracesSampleRate: 0.1,
-  // Taux d'échantillonnage des replays (10%)
+  enabled: true,
+  tracesSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
-  // Environnement
   environment: import.meta.env.MODE,
+  debug: false,
 });
 
-// Sentry.captureException(new Error("Test Sentry"));
-
-// On retire StrictMode pour éviter le double appel des useEffect en développement
 createRoot(document.getElementById("root")).render(<App />);
